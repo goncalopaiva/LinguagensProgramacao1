6d7f4798fb68b6c32bd03352f2d9f75c6833e5ed
Goncalo <esa.goncalo@live.com.pt>
2020-11-16 15:20:14 +0000

    adicionar tp09

---
+++ tp09.c
@@ -0,0 +1,5 @@
+//
+// Created by Goncalo on 11/16/2020.
+//
+
+#include "tp09.h"
---
+++ tp09.h
@@ -0,0 +1,8 @@
+//
+// Created by Goncalo on 11/16/2020.
+//
+
+#ifndef LPI_TP09_H
+#define LPI_TP09_H
+
+#endif //LPI_TP09_H
